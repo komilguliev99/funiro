5d362afdbc68d24e3faaab66d2cd116ac432e3fa
komilguliev99 <komil-guliev@mail.ru>
2022-09-27 13:37:00 +0300

    feat: gh-pages added

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className="App">
             <Header />
-            <Router basename="/">
+            <Router basename="/funiro">
                 <Routes>
                     <Route path="/" element={<Main />} />
                     <Route path="/" element={<Products />} />
